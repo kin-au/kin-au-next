c2611a53566a8a0cef03a81355546cffdc0da302
kin-au <kinnamau@gmail.com>
2022-05-28 22:06:30 +0100

    add tailwind, config favicon, headless-ui menu

--- components/mobileNav.tsx
+++ components/mobileNav.tsx
@@ -1,5 +1,25 @@
-import { useState } from "react";
+import { ReactNode, useState, forwardRef, ForwardedRef } from "react";
 import Link from "next/link";
+import { Popover } from "@headlessui/react";
+
+interface NavLinkProps {
+  href: string;
+  children: ReactNode;
+}
+
+const NavLink = forwardRef(function NavLink(
+  props: NavLinkProps,
+  ref: ForwardedRef<HTMLAnchorElement>
+) {
+  const { href, children, ...rest } = props;
+  return (
+    <Link href={href}>
+      <a ref={ref} {...rest}>
+        {children}
+      </a>
+    </Link>
+  );
+});
 
 const MobileNav = () => {
   const [showNav, setShowNav] = useState(false);
@@ -8,42 +28,29 @@ const MobileNav = () => {
   };
 
   return (
-    <>
-      {showNav && (
-        <nav>
-          <ul>
-            <li>
-              <Link href="/" onClick={toggleNav}>
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link href="/About" onClick={toggleNav}>
-                About Me
-              </Link>
-            </li>
-            <li>
-              <Link href="/Projects" onClick={toggleNav}>
-                Projects
-              </Link>
-            </li>
-            <li>
-              <Link href="/CV" onClick={toggleNav}>
-                CV
-              </Link>
-            </li>
-            <li className="bottomLink">
-              <Link href="/Contact" onClick={toggleNav}>
-                Contact
-              </Link>
-            </li>
-          </ul>
-        </nav>
-      )}
-      <button type="button" onClick={toggleNav}>
-        &#9776;
-      </button>
-    </>
+    <Popover className="relative">
+      <Popover.Button>&#9776;</Popover.Button>
+
+      <Popover.Panel className="absolute z-10">
+        <div className="grid grid-cols-2">
+          <Popover.Button as={NavLink} href="/">
+            Home
+          </Popover.Button>
+          <Popover.Button as={NavLink} href="/about">
+            About
+          </Popover.Button>
+          <Popover.Button as={NavLink} href="/projects">
+            Projects
+          </Popover.Button>
+          <Popover.Button as={NavLink} href="/cv">
+            CV
+          </Popover.Button>
+          <Popover.Button as={NavLink} href="/contact">
+            Contact
+          </Popover.Button>
+        </div>
+      </Popover.Panel>
+    </Popover>
   );
 };
 
---
+++ pages/about.tsx
@@ -0,0 +1,116 @@
+const About = () => {
+  return (
+    <main>
+      <div className="contentWrapper">
+        <h2>More about me</h2>
+        <div className="detailsContainer">
+          <details>
+            <summary>Timeline</summary>
+            <h4>2018</h4>
+            <p>
+              Oct – I was introduced to programming whilst working closely with
+              external software developers
+            </p>
+            <p>
+              Dec – Most of my free time was being spent teaching myself HTML,
+              CSS and JavaScript
+            </p>
+            <h4>2019</h4>
+            <p>
+              Feb – My interest in coding grew as I started attending meetup
+              events and conferences, and speaking to different people in the
+              tech industry
+            </p>
+            <p>May – I made it my goal to transition to tech</p>
+            <p>
+              Aug – Being accepted onto{" "}
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://www.foundersandcoders.com/"
+              >
+                Founders and Coders (FAC)
+              </a>
+              , a highly selective full stack JavaScript bootcamp, was a major
+              turning point in achieving this
+            </p>
+            <p>Oct – I quit my insurance job to start FAC</p>
+            <h4>2020</h4>
+            <p>
+              Jan – Whilst still at FAC, I began teaching students at{" "}
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://codebar.io"
+              >
+                Codebar.io
+              </a>
+            </p>
+            <p>Feb – Graduated from Founders and Coders</p>
+            <p>
+              Mar – Mentored current students at FAC, teaching the curriculum
+              and guiding teams through weekly projects
+            </p>
+            <p>
+              Jul – Launched my career in tech as a Frontend Developer at{" "}
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://smartway2.com"
+              >
+                Smartway2
+              </a>
+            </p>
+          </details>
+          <details>
+            <summary>Recent adventures and new learnings</summary>
+            <p>I&apos;m not really the type to stand still! Recently I have:</p>
+            <ul>
+              <li>
+                Continued to mentor (remotely) at{" "}
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://codebar.io"
+                >
+                  Codebar.io
+                </a>
+              </li>
+              <li>
+                Tried out different tech and tools like{" "}
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://www.typescriptlang.org"
+                >
+                  TypeScript
+                </a>{" "}
+                and{" "}
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://tailwindcss.com"
+                >
+                  Tailwind
+                </a>
+              </li>
+              <li>
+                Been playing with{" "}
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://www.arduino.cc"
+                >
+                  Arduino
+                </a>
+                , an electronics platform for hardware prototyping
+              </li>
+            </ul>
+          </details>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default About;
